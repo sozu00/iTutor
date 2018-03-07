a588020df8c7d12bad55875c2a915a67d288fd00
sozu00 <inhiguez.jesus@gmail.com>
2018-03-07 20:50:38 +0100

    commit inicial

---
+++ src/main/java/com/uca/jiniguez/itutor/ItutorApplication.java
@@ -0,0 +1,12 @@
+package com.uca.jiniguez.itutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItutorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uca/jiniguez/itutor/ItutorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uca.jiniguez.itutor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItutorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
